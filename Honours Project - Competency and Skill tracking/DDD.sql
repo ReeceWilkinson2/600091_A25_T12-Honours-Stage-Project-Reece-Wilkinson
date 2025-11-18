f655b94b4785c0e160aeb527ffb62fbf4b0b9731
ReeceWilkinson2 <reece.wilkinson-2022@hull.ac.uk>
2025-11-18 12:50:12 +0000

    Added project, database and gitignore

---
+++ Honours Project - Competency and Skill tracking/DDD.sql
@@ -0,0 +1,32 @@
+CREATE SCHEMA IF NOT EXISTS `HonoursSQL`;
+USE `HonoursSQL`;
+
+/*CREATE TABLE Departments
+(
+	Department varchar(10) NOT NULL PRIMARY KEY,
+    CONSTRAINT UniqueIDs UNIQUE (Department)
+);*/
+
+CREATE TABLE Staff
+(
+	StaffID varchar(10) NOT NULL PRIMARY KEY,
+    Username varchar(20) NOT NULL,
+    `Password` varchar(20) NOT NULL,
+	Email varchar(50) NOT NULL,
+    Surname varchar(40),
+    Forenames varchar(50) NOT NULL DEFAULT '',
+    `Role` varchar(10) NOT NULL,
+    CONSTRAINT UniqueIDs UNIQUE (StaffID)
+);
+
+CREATE TABLE Students
+(
+	StudentID varchar(10) NOT NULL PRIMARY KEY,
+    Username varchar(20) NOT NULL,
+    `Password` varchar(20) NOT NULL,
+	StEmail varchar(50) NOT NULL,
+    Surname varchar(40),
+    Forenames varchar(50) NOT NULL DEFAULT '',
+    `Role` varchar(10) NOT NULL,
+    CONSTRAINT UniqueIDs UNIQUE (StudentID)
+);
