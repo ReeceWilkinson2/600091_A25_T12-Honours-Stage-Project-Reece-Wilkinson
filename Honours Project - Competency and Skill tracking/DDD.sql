f5c358d6b15a9b88dae73d88c014b63ee0bf4337
ReeceWilkinson2 <reece.wilkinson-2022@hull.ac.uk>
2025-11-18 13:48:08 +0000

    Updated Database

--- Honours Project - Competency and Skill tracking/DDD.sql
+++ Honours Project - Competency and Skill tracking/DDD.sql
@@ -1,18 +1,18 @@
 CREATE SCHEMA IF NOT EXISTS `HonoursSQL`;
 USE `HonoursSQL`;
 
-/*CREATE TABLE Departments
+CREATE TABLE Departments
 (
 	Department varchar(10) NOT NULL PRIMARY KEY,
     CONSTRAINT UniqueIDs UNIQUE (Department)
-);*/
+);
 
 CREATE TABLE Staff
 (
 	StaffID varchar(10) NOT NULL PRIMARY KEY,
     Username varchar(20) NOT NULL,
     `Password` varchar(20) NOT NULL,
-	Email varchar(50) NOT NULL,
+	SEmail varchar(50) NOT NULL,
     Surname varchar(40),
     Forenames varchar(50) NOT NULL DEFAULT '',
     `Role` varchar(10) NOT NULL,
@@ -27,6 +27,19 @@ CREATE TABLE Students
 	StEmail varchar(50) NOT NULL,
     Surname varchar(40),
     Forenames varchar(50) NOT NULL DEFAULT '',
+    `Role` varchar(10) NOT NULL,
+    CONSTRAINT UniqueIDs UNIQUE (StudentID)
+);
+
+CREATE TABLE Employers
+(
+	EmployerID varchar(10) NOT NULL PRIMARY KEY,
+    Username varchar(20) NOT NULL,
+    `Password` varchar(20) NOT NULL,
+	EmEmail varchar(50) NOT NULL,
+    Surname varchar(40),
+    Forenames varchar(50) NOT NULL DEFAULT '',
+    PlaceOfEmployment varchar(50) NOT NULL DEFAULT '',
     `Role` varchar(10) NOT NULL,
     CONSTRAINT UniqueIDs UNIQUE (StudentID)
 );
