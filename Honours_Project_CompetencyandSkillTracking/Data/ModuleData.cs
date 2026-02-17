1ae0917ced489964b823ab0f010698d20078dd33
ReeceWilkinson2 <reece.wilkinson-2022@hull.ac.uk>
2026-02-17 21:41:00 +0000

    module class to house csv data

---
+++ Honours_Project_CompetencyandSkillTracking/Data/ModuleData.cs
@@ -0,0 +1,17 @@
+﻿namespace Honours_Project_CompetencyandSkillTracking.Data
+{
+    public class ModuleData
+    {
+        public string ModuleID { get; set; } = "";
+        public string ModuleCode { get; set; } = "";
+        public string ModuleName { get; set; } = "";
+        public string RouteID { get; set; } = "";
+        public string AwardID { get; set; } = "";
+        public string CBOYear { get; set; } = "";
+        public string Trimester { get; set; } = "";
+        public string SelectionStatus { get; set; } = "";
+        public string Level { get; set; } = "";
+        public string Credits { get; set; } = "";
+
+    }
+}
