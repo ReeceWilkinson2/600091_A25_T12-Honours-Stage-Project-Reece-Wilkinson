be4a066bcf613374ce3ac7edd0ec7ebd014a8dc6
ReeceWilkinson2 <reece.wilkinson-2022@hull.ac.uk>
2026-02-21 21:37:33 +0000

    Added Tab system for UI styling

---
+++ Honours_Project_CompetencyandSkillTracking/Components/UI Elements/TabItem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Honours_Project_CompetencyandSkillTracking.Components.UI_Elements
+{
+    public class TabItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public RenderFragment? ChildContent { get; set; }
+        public bool Active { get; set; }
+    }
+
+}
