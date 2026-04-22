50d03c9048c7451b10d61bd4da52d790bfec23e0
ReeceWilkinson2 <reece.wilkinson-2022@hull.ac.uk>
2026-04-22 12:43:17 +0100

    Moved grade calculations to a separate page

--- Honours_Project_CompetencyandSkillTracking/Data/AssignmentInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Honours_Project_CompetencyandSkillTracking.Data
-{
-    public class AssignmentInput
-    {
-        public string Name { get; set; } = "";
-        public decimal Score { get; set; } = 0;
-        public decimal Weighting { get; set; } = 0;
-    }
-}
